b4c16c7ba59c3571e6cb48695ec23a2b07979c03
sanasatsangi14 <sana.satsangi14@gmail.com>
2025-05-12 23:25:29 +0530

    server change kiye

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const ACTIONS = require('./src/Actions');
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('build'));
-app.use((req, res, next) => {
+app.use(express.static(path.join(__dirname, 'build')));
+/*app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});*/
+app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
